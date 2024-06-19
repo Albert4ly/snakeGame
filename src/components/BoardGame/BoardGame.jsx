35c72719811a53c6c69373ddc87f6b9d2ce08bb7
Albert <a91614830@gmail.com>
2024-06-19 18:12:10 +0200

    ref:board-to-game-component-rewrite

--- src/components/BoardGame/BoardGame.jsx
+++ src/components/BoardGame/BoardGame.jsx
@@ -1,23 +1,16 @@
+import { useRef } from 'react';
 import './BoardGame.css';
-import { ROWS, COLS, CELL } from "../../initGameConfig";
 
-function BoardGame() {
+const BoardOfGame = () => {
 
-   const cells = [];
-
-   for (let row = 0; row < ROWS; row++) {
-      for (let col = 0; col < COLS; col++) {
-         cells.push(<div className={'boardGame__cell'} />)
-      }
-   }
+   const canvasRef = useRef();
 
    return (
-      <div
-         style={{width: COLS*CELL, height: ROWS*CELL}}
-         className="boardGame">
-         {cells}
+      <div>
+         <canvas className="boardOfGame" ref={canvasRef} width={750} height={420} />
       </div>
-    )
+   )
+   
 }
 
-export default BoardGame;
+export default BoardOfGame;
